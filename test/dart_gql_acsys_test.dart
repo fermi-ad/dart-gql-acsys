faa63a65c8f9c5fe066ad45417fbd9fb19fb1a3b
Rich Neswold <neswold@fnal.gov>
2025-07-29 15:34:30 -0500

    :sparkles: create project
    This content is from the `dart create` command.

---
+++ test/dart_gql_acsys_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_gql_acsys/dart_gql_acsys.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
