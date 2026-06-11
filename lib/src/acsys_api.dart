f75cf60065b185fb420611006f0a17a95ee80b8e
Rich Neswold <neswold@fnal.gov>
2026-06-11 12:49:40 -0500

    :truck: move API types to separate module

---
+++ lib/src/acsys_api.dart
@@ -0,0 +1,104 @@
+import 'device_values.dart';
+
+final class Reading {
+  final int refId;
+  final DateTime timestamp;
+  final DeviceValue value;
+
+  const Reading({
+    required this.refId,
+    required this.timestamp,
+    required this.value,
+  });
+}
+
+final class PlotPoint {
+  final double t;
+  final DeviceValue value;
+
+  const PlotPoint({required this.t, required this.value});
+}
+
+final class PlotChannelData {
+  final String name;
+  final String units;
+  final String rate;
+  final int status;
+  final List<PlotPoint> points;
+
+  const PlotChannelData({
+    required this.name,
+    required this.units,
+    required this.rate,
+    this.status = 0,
+    this.points = const [],
+  });
+}
+
+final class PlotReply {
+  final String plotId;
+  final double requestTime;
+  final double? triggerTimestamp;
+  final String xAxisUnits;
+  final double? xAxisMin;
+  final double? xAxisMax;
+  final int? windowSize;
+  final List<PlotChannelData> data;
+
+  const PlotReply({
+    required this.plotId,
+    required this.requestTime,
+    this.triggerTimestamp,
+    required this.xAxisUnits,
+    this.xAxisMin,
+    this.xAxisMax,
+    this.windowSize,
+    required this.data,
+  });
+}
+
+final class SettingStatus {
+  final int facilityCode;
+  final int errorCode;
+
+  const SettingStatus({required this.facilityCode, required this.errorCode});
+}
+
+abstract interface class ACSysServiceAPI {
+  /// Takes a list of data acquisition strings and returns a stream that
+  /// provides readings for the requests.
+
+  Stream<Reading> monitorDevices(List<String> drfs);
+
+  /// Takes a list of device names and returns their current reading.
+
+  Future<List<Reading>> readDevices(List<String> devices);
+
+  /// Returns a stream which provides plot data for the devices specified in
+  /// the parameter list.
+  Stream<PlotReply> startPlot(
+    List<String> drfs, {
+    double? xMin,
+    double? xMax,
+    double? startTime,
+    double? endTime,
+    int? windowSize,
+    int? updateRate,
+    int? nAcquisitions,
+    int? triggerEvent,
+  });
+
+  /// Takes a device name and a value and sends a request to apply the value to
+  /// the device.
+  Future<SettingStatus> submit({
+    required String forDRF,
+    required DeviceValue newSetting,
+  });
+
+  /// Takes a device name and a value and sends a request to apply the value to
+  /// the device's digital control property.
+  Future<SettingStatus> sendCommand({
+    required String toDRF,
+    required String value,
+  });
+}
--- lib/src/acsys_service.dart
+++ lib/src/acsys_service.dart
@@ -18,7 +18,10 @@ import "package:gql_websocket_link/gql_websocket_link.dart";
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'device_values.dart';
+import 'acsys_api.dart';
 import 'status.dart';
+
+export 'acsys_api.dart';
 
 // Declare an exception type that's specific to the ACSys API.
 
@@ -54,70 +57,6 @@ class ACSysStatusException extends ACSysException {
     : super("ACNET status: [${status.facility} ${status.error}]");
 }
 
-final class Reading {
-  final int refId;
-  final DateTime timestamp;
-  final DeviceValue value;
-
-  const Reading({
-    required this.refId,
-    required this.timestamp,
-    required this.value,
-  });
-}
-
-final class SettingStatus {
-  final int facilityCode;
-  final int errorCode;
-
-  const SettingStatus({required this.facilityCode, required this.errorCode});
-}
-
-final class PlotPoint {
-  final double t;
-  final DeviceValue value;
-
-  const PlotPoint({required this.t, required this.value});
-}
-
-final class PlotChannelData {
-  final String name;
-  final String units;
-  final String rate;
-  final int status;
-  final List<PlotPoint> points;
-
-  const PlotChannelData({
-    required this.name,
-    required this.units,
-    required this.rate,
-    this.status = 0,
-    this.points = const [],
-  });
-}
-
-final class PlotReply {
-  final String plotId;
-  final double requestTime;
-  final double? triggerTimestamp;
-  final String xAxisUnits;
-  final double? xAxisMin;
-  final double? xAxisMax;
-  final int? windowSize;
-  final List<PlotChannelData> data;
-
-  const PlotReply({
-    required this.plotId,
-    required this.requestTime,
-    this.triggerTimestamp,
-    required this.xAxisUnits,
-    this.xAxisMin,
-    this.xAxisMax,
-    this.windowSize,
-    required this.data,
-  });
-}
-
 final class ChannelSettingSnapshot {
   final Color? lineColor;
   final int? markerIndex;
@@ -129,45 +68,6 @@ final class ChannelSettingSnapshot {
     this.markerIndex,
     this.yMin,
     this.yMax,
-  });
-}
-
-abstract interface class ACSysServiceAPI {
-  /// Takes a list of data acquisition strings and returns a stream that
-  /// provides readings for the requests.
-
-  Stream<Reading> monitorDevices(List<String> drfs);
-
-  /// Takes a list of device names and returns their current reading.
-
-  Future<List<Reading>> readDevices(List<String> devices);
-
-  /// Returns a stream which provides plot data for the devices specified in
-  /// the parameter list.
-  Stream<PlotReply> startPlot(
-    List<String> drfs, {
-    double? xMin,
-    double? xMax,
-    double? startTime,
-    double? endTime,
-    int? windowSize,
-    int? updateRate,
-    int? nAcquisitions,
-    int? triggerEvent,
-  });
-
-  /// Takes a device name and a value and sends a request to apply the value to
-  /// the device.
-  Future<SettingStatus> submit({
-    required String forDRF,
-    required DeviceValue newSetting,
-  });
-
-  /// Takes a device name and a value and sends a request to apply the value to
-  /// the device's digital control property.
-  Future<SettingStatus> sendCommand({
-    required String toDRF,
-    required String value,
   });
 }
 
